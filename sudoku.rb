1a31dcb1d144ae9303d774f8bb2fd6371b11608c
Bastion <snowy@ubuntu.(none)>
2013-03-09 01:08:02 -0600

    Using solved sudoku grid for tests, check_mat done

--- sudoku.rb
+++ sudoku.rb
@@ -5,28 +5,62 @@ class Sudoku
   end
 
   ##
-  # Place +num+ at +x+, +y+ in the sodoku puzzle
-  def place!(x, y, num)
-    @puzzle[y][x] = num
+  # Place +num+ at +col+, +row+ in the sodoku puzzle
+  def place!(col, row, num)
+    @puzzle[row][col] = num
   end
 
   ##
-  # Checks if the number at +x+, +y+ is legal
-  def check_pos(x,y)
-
+  # Checks if the number at +col+, +row+ is legal
+  def check_pos(col,row,num)
+    check_row row,num and check_col col,num
   end
 
   def self.check_row arr
     return false unless arr.size == 9
-    arr.each {|num| return false unless arr.count(num) == 1}
+    arr.each {|num| return false unless arr.count(num) == 1 and (0..9) === num}
   end
 
-  def check_row(y,num)
-    @puzzle[y].count(num) == 0
+  def check_row(row,num)
+    @puzzle[row].count(num) == 0
   end
 
-  def check_col(x,y)
+  def self.check_col arr, col
+    return false unless arr.size == 9
+    col_arr = []
+    9.times do |index|
+      col_arr << arr[index][col]
+    end
+    check_row col_arr
   end
+
+  def check_col(col,num)
+    col_arr = []
+    9.times do |index|
+      col_arr << @puzzle[index][col]
+    end
+    col_arr.count(num) == 0
+  end
+
+  def self.check_mat mat
+    mat_arr = []
+    mat.each do |row|
+      row.each do |num|
+        mat_arr << num
+      end
+    end
+    check_row mat_arr
+  end
+
+  def check_mat mat, num
+    mat_arr = []
+    9.times do |index|
+      row = 3 * (mat / 3) + index / 3
+      col = 3 * (mat % 3) + index % 3
+      mat_arr << @puzzle[row][col] 
+    end
+   mat_arr.count(num) == 0
+  end 
 
   ##
   # Checks the whole puzzle to see if it's a solution
@@ -52,25 +86,43 @@ fail unless Sudoku.check_row [1,2,3,4,5,6,7,8,9]
 fail if Sudoku.check_row [2,3,4,5,6,7,8,9]
 fail if Sudoku.check_row [2,2,3,4,5,6,7,8,9]
 fail if Sudoku.check_row [2,2,2,4,5,6,7,8,9]
+fail if Sudoku.check_row [2,3,4,5,6,7,8,9,10]
 
 test = []
 
 test_map = Sudoku.new(test)
 fail if test_map.valid?
 
-9.times do |i|
-  test << []
-  9.times do |j|
-    test[i] << (i + j ) % 9 + 1
-  end
-end
+test =
+[[4,1,7,5,2,3,6,9,8],
+ [2,5,3,9,8,6,1,4,7],
+ [9,8,6,1,7,4,3,2,5],
+ [6,9,1,8,5,7,4,3,2],
+ [5,3,2,4,6,9,8,7,1],
+ [7,4,8,2,3,1,5,6,9],
+ [3,7,9,6,1,5,2,8,4],
+ [8,6,5,7,4,2,9,1,3],
+ [1,2,4,3,9,8,7,5,6]]
 
 test_map = Sudoku.new(test)
 test_map.place!(0,0,0)
 fail unless test_map.puzzle[0][0] == 0
-fail unless test_map.check_row(0,1)
+fail unless test_map.check_row(0,4)
 fail unless test_map.valid?
-#test_map.place! 0,0,3
-#p test_map.puzzle
-#p test_map.check_row(0,0)
+test_map.place! 0,0,4
+
+fail unless Sudoku.check_col test_map.puzzle, 1
+test_map.place! 0,0,2
+fail if Sudoku.check_col test_map.puzzle, 0
+
+test_map.place! 0,0,0
+fail unless test_map.check_col 0,4
+test_map.place! 0,0,4
+fail if test_map.check_col 0,4
+
+fail if Sudoku.check_mat [[1,1,3],[4,5,6],[7,8,9]]
+fail unless Sudoku.check_mat [[1,2,3],[4,5,6],[7,8,9]]
+
+fail if test_map.check_mat 0,1
+
 puts "\e[32m Great job!\e[0m"
